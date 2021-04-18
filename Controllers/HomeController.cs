d3ddc6ed949c16cf380e68c03fc83972b4cab124
jerald macachor <jerald.macachor@gmail.com>
2021-04-17 23:20:38 -0700

    contact us validation

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace taskManager.Controllers
       return View();
     }
 
+    public IActionResult Contact()
+    {
+      return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- wwwroot/js/contact.js
+++ wwwroot/js/contact.js
@@ -1,68 +1,137 @@
 const dev = true;
-const contact = {
+const emailRegex = /\S+@\S+\.\S+/;
+// Holds the id of contact Page elements
+const contactID = {
   CONTACT_FORM: "contactForm",
   SENDER: "sender",
   EMAIL: "email",
   MESSAGE: "message",
   SEND_BTN: "sendBtn",
+  MODAL: "tmModal",
+  MODAL_MESSAGE: "modalMessage",
 }
+
+// Holds reference to HTML elements
 const contactPage = {};
 
 function init() {
-  console.log("Task Manager!")
+  console.log("Contact Page")
   initializeDomElements();
   setEventListeners();
-  if (dev) testFormData();
+  if (dev) { 
+    testContactData();
+    testRegex()
+  };
 }
 
 function initializeDomElements() {
   console.log('initializeDomElements()', 'getting DOM elements')
-  contactPage.contactForm = document.getElementById(contact.CONTACT_FORM);
-  contactPage.sender = document.getElementById(contact.SENDER);
-  contactPage.email = document.getElementById(contact.EMAIL);
-  contactPage.message = document.getElementById(contact.MESSAGE);
-  contactPage.sendBtn = document.getElementById(contact.SEND_BTN);
+  for (let key in contactID) {
+    const elementID = contactID[key];
+    contactPage[elementID] = document.getElementById(elementID);
+    console.log(`getting element ${elementID}, ${contactPage[elementID]}`);
+  }
+  console.log(contactPage);
 }
 
 function setEventListeners() {
   contactPage.contactForm.addEventListener('submit', submitContactForm);
+  $(contactPage.tmModal).hide();
 }
 
 function submitContactForm(e) {
   e.preventDefault();
-  validateForm();
+
+  const name = $(contactPage.sender).val();
+  const email = $(contactPage.email).val();
+  const msg = $(contactPage.email).val();
+  const messageToSend = {name, email, msg };
+  if (!validateForm(messageToSend)) return;
+
+  console.log(`sending message...`, messageToSend);
 }
 
-function validateForm() {
-  console.log("validateForm");
-  const validTitle = taskInputs[TASK_CONST.TITLE].val().length > 5;
-  if (!validTitle) {
-    console.error("Invalid Title Input");
-    alertInputError("Title must be more than 5 characters");
+function validateForm(message) {
+  console.log("validateForm()");
+
+  const validName = message.name.length > 0;
+  if (!validName) {
+    console.error("Invalid Sender Input");
+    alertInputError("Name enter a name.");
     return false;
   }
-  const validDescription = taskInputs[TASK_CONST.DESCRIPTION].val().length > 5;
-  if (!validDescription) {
+
+  const regexMatch = emailRegex.test(message.email);
+  const validEmail = regexMatch && (message.email.length > 0); 
+  if (!validEmail ) {
     console.error("Invalid Description Input");
-    alertInputError("Description must be more than 5 characters");
+    alertInputError("Must be a valid email");
     return false;
   }
-  const validStatus = taskInputs[TASK_CONST.STATUS].children()[0].selected == false;
-  if (!validStatus) {
+
+  const validMessage = message.msg.length > 5;
+  if (!validMessage) {
     console.error("Invalid Status Input");
-    alertInputError("Select a status for this task");
+    alertInputError("Message must be more than 5 characters.");
     return false;
   }
-  const valid = taskForm.checkValidity();
-  if(!valid) taskForm.classList.add('was-validated');
-  return valid;
+
+  return true;
 }
 
 function alertInputError(msg) {
-  modal.modal.show();
-  modal.message.text(msg)
+  $(contactPage.tmModal).text(msg);
+  $(contactPage.tmModal).show();
 }
 
 function alertInputErrorClose() {
-  modal.modal.hide();
+  $(contactPage.tmModal).hide();
+}
+
+init();
+
+function testContactData() {
+  console.log("testContactData()");
+  $(contactPage.sender).val("Jerald");
+  $(contactPage.email).val("jerald@maca.com");
+  $(contactPage.message).val("Hello World!!! How Are you???");
+}
+
+function testRegex() {
+  console.log("testRegex()");
+  const tests = [
+    {
+      email: "",
+      expected: false
+    },
+    {
+      email: "jerald",
+      expected: false
+    },
+    {
+      email: "jerald.macachor",
+      expected: false
+    },
+    {
+      email: "jerald.macachor@",
+      expected: false
+    },
+    {
+      email: "jerald.macachor@gmail",
+      expected: false
+    },
+    {
+      email: "jerald.macachor@gmail.com",
+      expected: true
+    },
+    {
+      email: "jerald.macachor@cable.comcast.com",
+      expected: true
+    }
+  ];
+  for (const test of tests) {
+    const passFail = emailRegex.test(test.email) === test.expected ? "Passed" : "Failed";
+    console.log(test.email, test.expected, passFail); 
+  }
+
 }
